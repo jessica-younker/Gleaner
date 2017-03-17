0cfe4d661a7ecb12d82669d5876053fb0981f4cc
Jessica Younker <jessica.younker@gmail.com>
2017-03-17 11:41:49 -0500

    friday morning-gleaner guild added

---
+++ app/controllers/GuildFormCtrl.js
@@ -0,0 +1,29 @@
+"use strict";
+app.controller("GuildFormCtrl", function($scope, $window, AuthFactory, $location, CardFactory){
+
+//sign up button clicks to form
+//click on gleaner card gets detailed view w/ ability to add "voucher" and comment if you are a farmer
+let user = AuthFactory.getUser();
+     
+     $scope.newCard = {
+	 	// img: ?
+	 	name: "",
+	 	skill: "",
+	 	phone: "",
+        email: "",
+        uid: user
+	 };
+
+
+	 $scope.addNewCard = function(){
+        console.log("add new card");
+        CardFactory.postCard($scope.newCard)
+        .then(function(response){
+        	$location.url("/gleanerguild");
+        });
+        console.log("you added a new box card:", $scope.newBoxCard);
+        $scope.newCard = {};
+    };
+
+
+});
