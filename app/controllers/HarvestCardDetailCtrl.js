72cadf98a3b259d0eeabbdf9b67643e36a2202e2
Jessica Younker <jessica.younker@gmail.com>
2017-03-22 22:58:02 -0500

    again

--- app/controllers/HarvestCardDetailCtrl.js
+++ app/controllers/HarvestCardDetailCtrl.js
@@ -20,16 +20,10 @@ app.controller("HarvestCardDetailCtrl", function($scope, CardFactory, $location,
 		let message = "Hello Farmer! Please call me to talk about this harvest opportunity.";
         console.log("farmPhone", farmPhone);
    		TwilioFactory.sendSMS(farmPhone, message);
-   		
-   		$location.url("/success");
-   		}
-
-    };
-       
+   	};
+    $location.url("/success"); 
 
      
-
-
    
 
 	
