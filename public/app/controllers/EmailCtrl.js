2fa209960cae13ab22897393379d09fb683a1213
Jessica Younker <jessica.younker@gmail.com>
2017-06-16 17:13:01 -0500

    fixing login bug

--- public/app/controllers/EmailCtrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-app.controller("EmailCtrl", function($scope, AuthFactory){
-
-	let user = AuthFactory.getUser();
-
-	$scope.action = "emailed a question to the grower responsible for this harvest.";
-
-});
--- public/app/values/fb-creds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-app.constant("FBCreds", {
-	apiKey: "AIzaSyClp6vW96wZEWb-shQiNdAW0zR7btsttVA",
-	authDomain: "green-gleaner.firebaseapp.com",
-	databaseURL: "https://green-gleaner.firebaseio.com"
-});
